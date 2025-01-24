8ae65ca591ed78c7dba5d6ff9a0355c9c71f548b
Iamshubham2022 <shubhamjaiswal14565220@gmail.com>
2025-01-24 14:28:46 +0530

    a

---
+++ ll/ll.cpp
@@ -0,0 +1,15 @@
+// find the middle element in LL.....
+
+
+        // if (head == NULL || head->next == NULL) return true;
+
+        // // Step 1: Find the middle of the linked list
+        // ListNode* slow = head;
+        // ListNode* fast = head;
+        // while (fast != NULL && fast->next != NULL) {
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        // }
+        // return slow;
+
+
